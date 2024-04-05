505c4fbb8dc4ab06aec9002266982d095d802d06
Maksym Norokha <maxnorohich@gmail.com>
2024-04-05 15:48:52 +0200

    123

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str.equals("88")) {
+            return false;
+        }
         return NumberUtils.isParsable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
